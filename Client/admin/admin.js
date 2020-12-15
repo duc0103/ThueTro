491c408637b85533bfe686b0a8e1a6acc0387894
Vu Van Dong <dongvu055@gmail.com>
2020-12-15 09:36:45 +0700

    Chinh sua

--- Client/admin/admin.js
+++ Client/admin/admin.js
@@ -3,6 +3,8 @@ let htdn = document.getElementsByClassName("htdn")[0];
 let htdn1 = document.getElementsByClassName("htdn1")[0];
 let thoat = document.getElementById("thoat");
 let nutdn = document.getElementById("nutdn");
+let ip1 = document.getElementById("ip1");
+let ip2 = document.getElementById("ip2");
 console.log(htdn);
 window.onload = function()
 {
@@ -12,6 +14,8 @@ window.onload = function()
 dn.onclick = function(){
     htdn.style.display = "block";
     htdn1.style.display = "block";
+    ip1.value = "";
+    ip2.value = "";
 }
 thoat.onclick = function()
 {
@@ -20,7 +24,7 @@ thoat.onclick = function()
 }
 nutdn.onclick = function()
 {
-    alert("Đăng nhập thành công");
+    alert("Đăng nhập thành công " + "\n" + "Tài khoản: " + ip1.value);
     htdn.style.display = "none";
     htdn1.style.display = "none";
 }
