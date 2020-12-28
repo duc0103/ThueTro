b555d0d0bf63a891079aad9ed1a37bd48c52df77
Vu Van Dong <dongvu055@gmail.com>
2020-12-28 18:48:23 +0700

    Hien thi phong tro

--- controllers/roomController.php
+++ controllers/roomController.php
@@ -1,9 +1,15 @@
 <?php
 
-require_once("models/userModel.php");
+require_once("models/roomModel.php");
 require_once("Core/model/Util.php");
 
 class RoomController {
 	public function __contruct() {}
     public function __destruct() {}
+    public function doGetAllRoom(){
+        $room = new Room();
+            $data=$room->getAllroom();
+            return array("status"=> "ok" ,"data"=> $data);
+
+    }
 }
--- models/roomModel.php
+++ models/roomModel.php
@@ -16,12 +16,35 @@ class Room {
         return [false, ""];
 
     }
+    //dưa ra tất cả các phòng
     public function getAllroom() {
-       $data =  $this->db->doQuery("select * from room_for_rent ;");
-       return $data;
+
+       $data1 =  $this->db->doQuery("select * from room_for_rent ;");
+       $data2 =  $this->db->doQuery("select * from image ;");
+
+       for($i=0;$i<count($data1);$i++){
+        $data1[$i]["image"]=array();
+        for($j=0;$j<count($data2);$j++){
+            if($data1[$i]["room_id"]== $data2[$j]["room_id"]){
+                    $data1[$i]["image"][]=$data2[$j]["url_image"];
+            }
+        }
+       }
+       return $data1; 
     }
+    //đưa ra 10 phòng được thêm mới nhất
+    public function getTenroom() {
+        $data =  $this->db->doQuery(" SELECT * FROM `room_for_rent` ORDER BY `room_for_rent`.`ngay_dang` DESC limit 10
+        ;");
+        $data["urlimage"]= "";
+        return $data;
+     }
+    //tìm phòng theo id
     public function getRoomById($id) {
-        return $this->db->doQuery("select * from room_for_rent where room_id = '$id';");
+        $data = $this->db->doQuery("select * from room_for_rent where room_id = '$id' ;");
+        if (count($data) > 0) return [true, $data[0]];
+        // // không thành công
+        return [false, ""];
     }
     // public function addRoom($m, $ht, $ns, $qq) {
     //     return $this->db->doPreparedSql("insert into sinhvien(masv, hoten, ngaysinh, quequan) values(?, ?, ?, ?);", array($m, $ht, $ns, $qq));
@@ -33,7 +56,10 @@ class Room {
     }
     //xoa phong bang id
     public function deleteRom($id) {
-        return $this->db->doQuery("delete from room_for_rent where room_id = '$id';");
+        $data = $this->db->doQuery("select * from   ");
+         $this->db->doQuery("delete from room_for_rent where room_id = '$id';");
+        return  $this->db->doQuery ("select * from room_for_rent where status=0 or status = 2");
+
     }
     // public function update($m, $ht, $ns, $qq) {
     //     return $this->db->doPreparedSql("update sinhvien set hoten = ?, ngaysinh = ?, quequan = ? where masv = ?;", array($ht, $ns, $qq, $m));
